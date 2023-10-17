fe49b73fd869bd265b13dc22562cc1f9fcdc6f20
Aman Danidhariya <amandanidhariya007@gmail.com>
2023-10-17 19:39:52 +0530

    fetch data with getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,44 @@
-import MeetupList from "\u0016@component/components/meetups/MeetupList"
+import MeetupList from "\u0016@component/components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
   {
-    id: 'm1',
-    title: 'A First Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!'
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
   },
   {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 10, 12345 Some City',
-    description: 'This is a second meetup!'
-  }
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 10, 12345 Some City",
+    description: "This is a second meetup!",
+  },
 ];
 
-export default function Home() {
+const Home = (props) => {
   return (
     <>
-      <MeetupList meetups={DUMMY_MEETUPS}/>
+      <MeetupList meetups={props.meetups} />
     </>
-  )
+  );
+};
+
+export async function getStaticProps(){
+//the code which we written here never execute on clientSide
+return{
+  props:{
+    //dummy_meetup loaded and prepared in getStaticProps &
+    //  then they set as props for this page component
+    meetups: DUMMY_MEETUPS,
+  }
+};
 }
+
+
+
+
+export default Home;
